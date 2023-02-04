a71213f65c334ef8e183cd49250b5bb1dc97d4af
Pit Ace <pitacerocket@gmail.com>
2023-02-04 05:04:41 +0300

    Create users/forms.py, add new CreationForm and new ChangeForm for myCustomUser

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import MyCustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = MyCustomUser
+        fields = ("email",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = MyCustomUser
+        fields = ("email",)
+        
