7e4889f8174b9f6d905f9b17ee7eeba03eab8d6c
Pit Ace <pitacerocket@gmail.com>
2023-02-04 04:50:43 +0300

    update users/models.py : create MyCustomUser class

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,User
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class MyCustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_("username"), max_length=150) #, unique=True)
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    USERNAME_FILED = "email"
+    REQUIRED_FIELDS = ["username"]
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
