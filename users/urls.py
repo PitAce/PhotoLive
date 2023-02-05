c9f93e58e244b980b4a9379e528a0405cf02dfb7
Pit Ace <pitacerocket@gmail.com>
2023-02-06 02:36:07 +0300

    upgrate users/views.py : registration_view corrected cleaned_data() on cleaned_data[]

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from .views import registration_view
 
 urlpatterns = [
-    path('', include('users.urls')),
     path('register/', registration_view, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -5,15 +5,15 @@ from .forms import RegistrationForm
 
 def registration_view(request):
 
-    template_name = 'register.html'
+    template_name = 'users/register.html'
     context = {}
 
     if request.POST:
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            email = form.cleaned_data("email")
-            password = form.cleaned_data("password1")
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password1"]
             user = authenticate(email=email, password=password)
             login(request, user)
             return redirect("base.html")
