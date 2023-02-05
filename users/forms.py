e69e03541a2e942616e28b82844755a9ac99b703
Pit Ace <pitacerocket@gmail.com>
2023-02-05 22:30:46 +0300

    add RegistrationForm in to users/forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import MyCustomUser
-
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -8,8 +8,14 @@ class CustomUserCreationForm(UserCreationForm):
         model = MyCustomUser
         fields = ("email",)
 
-
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = MyCustomUser
         fields = ("email",)
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=60, help_text="Need add a vailed email address")
+
+    class Meta:
+        model = MyCustomUser
+        fields = ("email", "username", "password1", "password2")
