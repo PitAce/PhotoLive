e4957631285e769f7caf0452b38aaa392caa8a10
Pit Ace <pitacerocket@gmail.com>
2023-02-06 23:54:23 +0300

    Add url 'login/' in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 
 from .views import (
     registration_view,
-    logout_view
+    logout_view,
+    login_view
 )
 
 urlpatterns = [
-    path('register/', registration_view, name="register"),
+    path('register/', registration_view, name='register'),
+    path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 ]
