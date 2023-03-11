a6966b79e00321bf3982bf89370de46346d02333
Pit Ace <pitacerocket@gmail.com>
2023-03-11 16:00:04 +0300

    changed 'get_avatar_img' in website/pipeline.py

--- website/pipeline.py
+++ website/pipeline.py
@@ -1,14 +1,17 @@
-from urllib.request import urlopen
-from django.core.files.base import ContentFile
+from PIL import Image
+import requests
+from io import BytesIO
+from urllib.parse import urlparse
 
 def get_avatar_img(backend, user, response, *args, **kwargs):
-    if backend.name == 'github':
-        avatar_img = urlopen(response['avatar_url'])
+    if backend.name == 'github' and not user.userprofile.avatar:
+        # import pdb
+        # pdb.set_trace()
+        avatar_url = urlparse(response['avatar_url'])
+        filename = avatar_url.path.split('/')[-1]
 
-        # save picture if user haven't avatar
-        if not user.userprofile.avatar:
-            user.userprofile.avatar.save(
-                "{0}.jpg".format(user.username),
-                ContentFile(avatar_img.read()),
-            )
+        avatar_response = requests.get(response['avatar_url'])
+        img_io = BytesIO(avatar_response.content)
+        img = Image.open(img_io)
+        user.userprofile.avatar.save(f"{filename}.{img.format.lower()}", img_io)
 
