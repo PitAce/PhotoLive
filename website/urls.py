71848ba4a79770639e98333a02ecc8f270c11bd7
Pit Ace <pitacerocket@gmail.com>
2023-03-15 23:39:15 +0300

    Added path to 'profile' and 'edit_profile' in website/urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from .views import (
     registration_view,
@@ -11,6 +12,8 @@ urlpatterns = [
     path('register/', registration_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('profile/', TemplateView.as_view(template_name='website/profile.html'), name='profile'),
+    path('edit_profile/', edit_user_profile_view, name='edit_profile'),
     path('social-auth/', include('social_django.urls', namespace='social')),
 
 ]
