c53dfad8ec6c0c6881c594f61ca52e28051ddc49
Pit Ace <pitacerocket@gmail.com>
2023-03-15 23:31:06 +0300

    Add UpdateUserForm in website/forms.py

--- website/forms.py
+++ website/forms.py
@@ -52,4 +52,16 @@ class AuthenticationUserForm(forms.Form):
         fields = ("email", "password",)
 
 
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(required=True,
+                               label="Username:",
+                               widget=forms.TextInput(attrs={'class': 'form-control'}),)
+    email = forms.EmailField(max_length=60,
+                             required=True,
+                             label="Email:",
+                             widget=forms.EmailInput(attrs={"class": "form-control"}),)
+
+    class Meta:
+        model = MyCustomUser
+        fields = ['username', 'email']
 
