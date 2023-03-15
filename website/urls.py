f250f4a68a518aa5a18a2f3df56ce03897994571
Pit Ace <pitacerocket@gmail.com>
2023-03-16 00:06:59 +0300

    Added function edit_user_profile_view in website/views.py

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from .views import (
-    registration_view,
-    logout_view,
-    login_view,
-)
+from .views import (registration_view, logout_view, login_view, edit_user_profile_view,)
 
 urlpatterns = [
 
@@ -14,6 +10,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('profile/', TemplateView.as_view(template_name='website/profile.html'), name='profile'),
     path('edit_profile/', edit_user_profile_view, name='edit_profile'),
+
     path('social-auth/', include('social_django.urls', namespace='social')),
 
 ]
