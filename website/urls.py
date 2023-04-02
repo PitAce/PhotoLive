f0e873798c75ebfe9110e106457f26507a0a3016
Pit Ace <pitacerocket@gmail.com>
2023-04-03 02:07:16 +0300

    Added new path in urls.py for details_photo

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 from .views import (registration_view, logout_view,
                     login_view, edit_user_profile_view,
-                    user_profile, base_view)
+                    user_profile, base_view, show_details_photo)
 
 urlpatterns = [
     path('', base_view),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/', user_profile, name='profile'),
+    path('photo/<int:pk>/', show_details_photo, name='details_photo'),
     #path('profile/', TemplateView.as_view(template_name='website/profile.html'), name='profile'),
     path('edit_profile/', edit_user_profile_view, name='edit_profile'),
 
