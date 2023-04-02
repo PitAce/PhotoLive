79fdb41e53d401163dd086b73a6b01b590f604ba
Pit Ace <pitacerocket@gmail.com>
2023-04-03 01:13:52 +0300

    added atribut: blank=False for some fields in UserImages model and maked new migration

--- app_model/models/user_images.py
+++ app_model/models/user_images.py
@@ -6,10 +6,10 @@ from django.core.validators import FileExtensionValidator
 
 class UserImages(models.Model):
     user = models.ForeignKey(MyCustomUser, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, blank=True)
-    image = models.ImageField(upload_to='images/', blank=True, validators=[FileExtensionValidator(allowed_extensions=['jpeg','jpg'])])
+    title = models.CharField(max_length=100, blank=False)
+    image = models.ImageField(upload_to='images/', blank=False, validators=[FileExtensionValidator(allowed_extensions=['jpeg','jpg'])])
     created = models.DateTimeField(auto_now_add=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=False)
 
     def __str__(self):
         return self.title
