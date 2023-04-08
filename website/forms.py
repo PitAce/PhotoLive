a2cc16700bea64076633c0ab3e4fb47b5a670647
Pit Ace <pitacerocket@gmail.com>
2023-04-09 00:12:08 +0300

    RegistrationForm moved to /form/user/registration.py

--- website/forms.py
+++ website/forms.py
@@ -4,29 +4,29 @@ from django.contrib.auth.forms import UserCreationForm
 from app_model.models import MyCustomUser, UserProfile, UserPhoto
 
 
-class RegistrationForm(UserCreationForm):
-    username = forms.CharField(
-        help_text="Enter your name",
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your Name'})
-    )
-    email = forms.EmailField(
-        max_length=60,
-        help_text="Enter a vailed email address",
-        widget=forms.EmailInput(attrs={"class": "form-control", 'placeholder': 'Email'}),
-    )
-    password1 = forms.CharField(
-        help_text="Enter a password",
-        widget=forms.PasswordInput(attrs={"class": "form-control", 'placeholder': 'Password1'}),
-    )
-
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control", 'placeholder': 'Password2'}),
-        help_text="Enter the same password as before"
-    )
-
-    class Meta:
-        model = MyCustomUser
-        fields = ("username", "email", "password1", "password2",)
+# class RegistrationForm(UserCreationForm):
+#     username = forms.CharField(
+#         help_text="Enter your name",
+#         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your Name'})
+#     )
+#     email = forms.EmailField(
+#         max_length=60,
+#         help_text="Enter a vailed email address",
+#         widget=forms.EmailInput(attrs={"class": "form-control", 'placeholder': 'Email'}),
+#     )
+#     password1 = forms.CharField(
+#         help_text="Enter a password",
+#         widget=forms.PasswordInput(attrs={"class": "form-control", 'placeholder': 'Password1'}),
+#     )
+#
+#     password2 = forms.CharField(
+#         widget=forms.PasswordInput(attrs={"class": "form-control", 'placeholder': 'Password2'}),
+#         help_text="Enter the same password as before"
+#     )
+#
+#     class Meta:
+#         model = MyCustomUser
+#         fields = ("username", "email", "password1", "password2",)
 
     # def __init__(self, *args, **kwargs):
     #     super(RegistrationForm, self).__init__(*args, **kwargs)
