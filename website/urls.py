b0dd279efd71f2743c06e0e1cbd6fcbbf69734a8
Pit Ace <pitacerocket@gmail.com>
2023-04-09 23:44:04 +0300

    converting FBView: login_view, in to CBV: LoginView

--- website/urls.py
+++ website/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from .views import (UserLogoutView, BaseView, registration_view, ShowDetailsPhoto,
-                    login_view, edit_user_profile_view,
+from .views import (UserLogoutView, BaseView, LoginView,
+                    registration_view, ShowDetailsPhoto,
+                    edit_user_profile_view,
                     user_profile)
 
 urlpatterns = [
     path('', BaseView.as_view()),
     path('register/', registration_view, name='register'),
-    path('login/', login_view, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('profile/', user_profile, name='profile'),
     path('photo/<int:pk>/', ShowDetailsPhoto.as_view(), name='details_photo'),
--- website/views/login.py
+++ website/views/login.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.views import View
 
 from website.forms import AuthenticationUserForm
 
-
-def login_view(request):
+class LoginView(View):
     template_name = 'website/login.html'
     context = {}
-    if request.POST:
+
+    def get(self, request):
+        form = AuthenticationUserForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+    def post(self, request):
         form = AuthenticationUserForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
@@ -19,11 +24,31 @@ def login_view(request):
             else:
                 context = {'form': form,
                            'error_auth': 'Enter correct data!'}
-                return render(request, template_name, context)
+                return render(request, self.template_name, context)
         else:
             context = {'form': form}
-            return render(request, template_name, context)
-    else:
-        form = AuthenticationUserForm()
-        context = {'form': form}
-        return render(request, template_name, context)
+            return render(request, self.template_name, context)
+
+# def login_view(request):
+#     template_name = 'website/login.html'
+#     context = {}
+#     if request.POST:
+#         form = AuthenticationUserForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email']
+#             password = form.cleaned_data['password']
+#             user = authenticate(email=email, password=password)
+#             if user:
+#                 login(request, user)
+#                 return redirect("base")
+#             else:
+#                 context = {'form': form,
+#                            'error_auth': 'Enter correct data!'}
+#                 return render(request, template_name, context)
+#         else:
+#             context = {'form': form}
+#             return render(request, template_name, context)
+#     else:
+#         form = AuthenticationUserForm()
+#         context = {'form': form}
+#         return render(request, template_name, context)
