412449918c70232d0eaee0423d52e21a2552a519
Pit Ace <pitacerocket@gmail.com>
2023-04-09 22:59:40 +0300

    converting FBView: show_details_photo, in to CBV

--- website/views/show_details_photo.py
+++ website/views/show_details_photo.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
 from app_model.models import Photo
 
-def show_details_photo(request, pk):
-    photo = Photo.objects.get(pk=pk)
-    return render(request, 'website/details_photo.html', {'photo': photo})
+
+class ShowDetailsPhoto(ListView):
+    model = Photo
+    template_name = 'website/details_photo.html'
+    context_object_name = 'photo'
+
+    def get_queryset(self):
+        return Photo.objects.get(id=self.kwargs['pk'])
+
+# class ShowDetailsPhoto(TemplateView):
+#     template_name = 'website/details_photo.html'
+
+#     def get(self, request, *args, **kwargs):
+#         photo = Photo.objects.get(id=kwargs['pk'])
+#         return render(request, self.template_name, {'photo': photo})
