1d0496869651dd02b97074f9e74a6235b46f65fc
Pit Ace <pitacerocket@gmail.com>
2023-04-12 22:45:28 +0300

    renamed and moved model: profile.py to new directory: ../user/profile/model.py

--- app_model/models/__init__.py
+++ app_model/models/__init__.py
@@ -1,3 +1,3 @@
 from app_model.models.user.my_custom_user import MyCustomUser
-from app_model.models.user.profile import UserProfile
+from app_model.models.user.profile.model import UserProfile
 from app_model.models.photo.model import Photo
