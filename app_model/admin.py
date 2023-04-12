9e7c1c325cde073aae06953a4a76354771078ee1
Pit Ace <pitacerocket@gmail.com>
2023-04-13 00:55:59 +0300

    renamed and moved forms: CustomUserChangeForm, CustomUserCreationForm in to new dir: forms/user/

--- app_model/admin.py
+++ app_model/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import UserCreateForm, UserUpdateForm
 from .models import MyCustomUser, UserProfile
 from .models.photo.model import Photo
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+    add_form = UserCreateForm
+    form = UserUpdateForm
     model = MyCustomUser
     list_display = ("username", "email", "is_staff", "is_active",)
     list_filter = ("username", "email", "is_staff", "is_active",)
     fieldsets = (
-        (None, {"fields": ("username", "email", "password")}),
+        (None, {"fields": ("username", "email", "password", "date_joined")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
     )
     add_fieldsets = (
