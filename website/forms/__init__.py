c26a8dcb21b98059e55f87833da19466f90d31d4
Pit Ace <pitacerocket@gmail.com>
2023-05-28 23:57:24 +0300

    Added CommentForm to __init__ form

--- website/forms/__init__.py
+++ website/forms/__init__.py
@@ -3,3 +3,4 @@ from website.forms.user.authentication import AuthenticationUserForm
 from website.forms.user.update_user_data import UpdateUserForm
 from website.forms.user.avatar import UpdateUserAvatarForm
 from website.forms.photo.upload import UploadPhotoForm
+from website.forms.comment.comment_form import CommentForm
