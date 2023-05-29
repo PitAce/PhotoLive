fd024c316fe7b5a0c14109e149ad5141da324969
Pit Ace <pitacerocket@gmail.com>
2023-05-30 00:37:21 +0300

    edit comment_form.py: added widgets

--- website/forms/comment/comment_form.py
+++ website/forms/comment/comment_form.py
@@ -3,9 +3,7 @@ from app_model.models.photo.comment.model import Comment
 
 
 class CommentForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'class': 'form-control', 'style': 'background:#fff', 'placeholder': 'New comment...'}), label='')
     class Meta:
         model = Comment
         fields = ['text']
-        # widgets = {
-        #     'text': forms.TextInput(attrs={'class': 'form-control'}),
-        # }
