6cbc636a5808a0719b027dfc82d72ee1f1766538
Pit Ace <pitacerocket@gmail.com>
2023-05-30 23:34:52 +0300

    Added path for CommentView (add_comments)

--- website/urls.py
+++ website/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('profile/', login_required(UploadPhotoView.as_view()), name='profile'),
 
     path('photo/<int:pk>/', ShowDetailsPhotoView.as_view(), name='details_photo'),
-    path('photo/comments/<int:id>/', CommentView.as_view(), name='add_comment'),
+    path('photo/<int:photo_id>/<int:comment_id>/<slug:content_object_key>/', CommentView.as_view(), name='add_comment'),
     path('edit_profile/', login_required(EditUserProfileView.as_view()), name='edit_profile'),
     path('like/<int:photo_pk>/', LikeView.as_view(), name='like'),
 
