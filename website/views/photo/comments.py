1b0eb288aa6ffa5ab5bc2e37f89c5aac2c686821
Pit Ace <pitacerocket@gmail.com>
2023-06-10 14:06:05 +0300

    added 'acceptable_types' and edited the way for getting 'content_object'

--- website/views/photo/comments.py
+++ website/views/photo/comments.py
@@ -1,5 +1,3 @@
-# from django.contrib.auth.decorators import login_required
-from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render, redirect
 from django.views import View
 from app_model.models import Comment, Photo
@@ -9,16 +7,14 @@ class CommentView(View):
 
     def post(self, request, *args, **kwargs):
         form = CommentForm(request.POST)
-        # import pdb
-        # pdb.set_trace()
-        if kwargs['content_object_key'] == 'photo':
-            content_object = Photo.objects.get(id=kwargs['photo_id'])
-        else:
-            content_object = Comment.objects.get(id=kwargs['comment_id'])
+        acceptable_types = {"photo": Photo,
+                            "comment": Comment}
 
+        content_object = acceptable_types[kwargs['content_type']].objects.get(id=kwargs['content_id'])
         if form.is_valid():
             Comment.objects.create(content_object=content_object,
                                         photo_id=kwargs["photo_id"],
                                         text=form.cleaned_data['text'],
                                         author_id=request.user.id,)
         return redirect('details_photo', pk=kwargs['photo_id'])
+
