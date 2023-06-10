32895a6cfeca7b40a59ef8de15a7396d09a39ab4
Pit Ace <pitacerocket@gmail.com>
2023-06-10 13:51:43 +0300

    urls.py: changed 'path' for CommentView

--- website/urls.py
+++ website/urls.py
@@ -14,12 +14,11 @@ urlpatterns = [
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('profile/', login_required(UploadPhotoView.as_view()), name='profile'),
 
-    path('photo/<int:pk>/', ShowDetailsPhotoView.as_view(), name='details_photo'),
-    path('photo/<int:photo_id>/<int:comment_id>/<slug:content_object_key>/', CommentView.as_view(), name='add_comment'),
     path('edit_profile/', login_required(EditUserProfileView.as_view()), name='edit_profile'),
+
+    path('photo/<int:pk>/', ShowDetailsPhotoView.as_view(), name='details_photo'),
+    path('photo/<int:photo_id>/<slug:content_type>/<int:content_id>/comments', CommentView.as_view(), name='add_comment'),
     path('like/<int:photo_pk>/', LikeView.as_view(), name='like'),
-
-
 
     path('social-auth/', include('social_django.urls', namespace='social')),
 ]
