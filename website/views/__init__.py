7def369c66f8afacc08d4ebfa045bc85f5046567
Pit Ace <pitacerocket@gmail.com>
2023-07-04 12:09:25 +0300

    deleted show_details_photo_view, deleted 'post' in upload_photo.py

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -1,11 +1,10 @@
 # import website.views
 # from base import BaseView
-from website.views.main import BaseView
 from website.views.session.logout import UserLogoutView
 from website.views.user.registration import UserRegistrationView #registration_view
 from website.views.session.login import UserLoginView
-from website.views.photo.upload_photo import UploadPhotoView
+from website.views.photos.upload_photo import UploadPhotoView
 from website.views.user.edit_user_profile import EditUserProfileView #edit_user_profile_view
-from website.views.photo.show_details_photo import ShowDetailsPhotoView
-from website.views.photo.likes import LikeView
-from website.views.photo.comments import CommentView
+
+from website.views.photos.likes import LikeView
+from website.views.photos.comments import CommentView
--- website/views/photos/upload_photo.py
+++ website/views/photos/upload_photo.py
@@ -1,37 +1,10 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
-
-from app_model.models import UserProfile
 from website.forms import UploadPhotoForm
 
 class UploadPhotoView(View):
     template_name = 'website/user/profile.html'
-
     def get(self, request):
-        # user_avatar = UserProfile.objects.get(user_id=request.user.id)
         form = UploadPhotoForm()
         return render(request, self.template_name, {'form': form})
 
-    # def post(self, request):
-    #     # user_avatar = UserProfile.objects.get(user_id=request.user.id)
-    #     form = UploadPhotoForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         user_img = form.save(commit=False)
-    #         user_img.user = request.user
-    #         user_img.save()
-    #         return redirect(to='base')
-
-# @login_required
-# def user_profile(request):
-#     user_avatar = UserProfile.objects.get(user_id=request.user.id)
-#     if request.method == 'POST':
-#         form = UploadPhotoForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             user_img = form.save(commit=False)
-#             user_img.user = request.user
-#             user_img.save()
-#             return redirect(to='base')
-#     else:
-#         form = UploadPhotoForm()
-#     return render(request, 'website/profile.html', {'user_avatar': user_avatar, 'form': form})
